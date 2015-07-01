8d1761b14f45229458aa3883267f8069daa1e583
Adam Gomes <adamgomes89@gmail.com>
2015-07-01 17:24:25 -0500

    Removed Projects

--- WeatherReport/src/weathermap/WeatherService.java
+++
@@ -1,8 +0,0 @@
-package weathermap;
-
-import org.json.JSONException;
-import java.io.IOException;
-
-public interface WeatherService {
-     WeatherData getWeatherForCity(String city) throws IOException, JSONException;
-}
